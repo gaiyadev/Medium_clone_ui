add2da2095c97e6d909778226b619a08cc77df35
gaiyadev <gaiyaobed94@gmail.com>
2020-09-07 19:20:47 +0100

    Add: Folder structures and startup files

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Medium'),
+      ),
+      body: Center(
+        child: Text('cool'),
+      ),
+    );
+  }
+}
